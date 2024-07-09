a969f067e27307ccd872a638e690ed6395458149
kore <korinajuric03@gmail.com>
2024-07-09 18:15:42 +0200

    star rating
    kad vec kasnim evo i bonus hihi

--- tvshows/src/components/features/review/ReviewItem.tsx
+++ tvshows/src/components/features/review/ReviewItem.tsx
@@ -1,7 +1,8 @@
-import { IReview } from "@/typings/review.types";
-import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { IReview } from '@/typings/review.types';
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react';
+import StarRating from './StarRating';
 
-const maxRating = "5";
+const maxRating = '5';
 
 interface IReviewItemProps {
   reviewItem: IReview;
@@ -9,47 +10,55 @@ interface IReviewItemProps {
 }
 
 export const ReviewItem = ({ reviewItem, onDelete }: IReviewItemProps) => {
-  const userName = reviewItem.email?.split("@")[0] ?? "anonymous";
+  const userName = reviewItem.email?.split('@')[0] ?? 'anonymous';
   /* Nije mi baš bilo jasno otkud bismo povlačili email i avatar korisnika 
   budući da nema nikakvog logina tako da se svi po defaultu prikazuju kao anonymousi bez profilne. 
   Iz maila se uzima samo dio prije '@' jer mi je prirodnije (a možda i sigurnije) prikazati email adresu bez domene.*/
 
   return (
-    <Box backgroundColor="#380a88" borderRadius="lg" p={5} textAlign="left">
-      <Box display="flex" alignItems="center" padding="20px">
+    <Box backgroundColor='#380a88' borderRadius='lg' p={5} textAlign='left'>
+      <Box display='flex' alignItems='center' padding='20px'>
         <Image
-          borderRadius="100px"
-          fallbackSrc="/images/placeholder_user.jpg"
-          boxSize="30px"
+          borderRadius='100px'
+          fallbackSrc='/images/placeholder_user.jpg'
+          boxSize='30px'
           src={reviewItem.avatar}
-          alt="User avatar"
-          mr="2"
-          marginRight="20px"
+          alt='User avatar'
+          mr='2'
+          marginRight='20px'
         />
-        <Text fontSize="1em">{userName}</Text>
+        <Text fontSize='1em'>{userName}</Text>
       </Box>
       {reviewItem.review && (
-        <Box mt="1" as="p" color="aliceblue" padding="0px 20px">
+        <Box mt='1' as='p' color='aliceblue' padding='0px 20px'>
           {reviewItem.review}
         </Box>
       )}
-      <Box
-        mt="2"
-        as="p"
-        lineHeight="tight"
+      {/* <Box
+        mt='2'
+        as='p'
+        lineHeight='tight'
         noOfLines={1}
-        color="aliceblue"
-        padding="15px 20px 15px 20px"
-        fontWeight="semibold"
+        color='aliceblue'
+        padding='15px 20px 15px 20px'
+        fontWeight='semibold'
       >
         {reviewItem.rating
           ? `${reviewItem.rating} / ${maxRating}`
-          : "No rating"}
-      </Box>
+          : 'No rating'}
+      </Box> */}
+      <Flex align='center' flexDirection='row' mt={5} ml={5}>
+        <StarRating defaultValue={reviewItem.rating} onChange={() => {}} mode={1} />
+        <Text ml={5} mt={1} as='b'>
+          {reviewItem.rating
+            ? `${reviewItem.rating} / ${maxRating}`
+            : 'No rating'}
+        </Text>
+      </Flex>
       <Box
-        display="flex"
-        justifyContent="flex-end"
-        padding="0px 20px 15px 20px"
+        display='flex'
+        justifyContent='flex-end'
+        padding='0px 20px 15px 20px'
       >
         <Button onClick={() => onDelete(reviewItem)}>Remove</Button>
       </Box>
--- tvshows/src/components/features/shows/ReviewForm.tsx
+++ tvshows/src/components/features/shows/ReviewForm.tsx
@@ -1,34 +1,37 @@
-import { IReview } from "@/typings/review.types";
-import { Button, Flex, Input, Textarea, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { IReview } from '@/typings/review.types';
+import { Button, Flex, Input, Textarea, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import StarRating from '../review/StarRating';
 
 interface IReviewFormProps {
   onAdd: (review: IReview) => void;
 }
 
 export const ReviewForm = ({ onAdd }: IReviewFormProps) => {
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState('');
+  const [rating, setRating] = useState(0);
   
   const onClickHandler = () => {
     const reviewInput = document.getElementById(
-      "review-input"
+      'review-input'
     ) as HTMLInputElement;
-    const ratingInput = document.getElementById(
-      "rating-input"
-    ) as HTMLInputElement;
+    // const ratingInput = document.getElementById(
+    //   'rating-input'
+    // ) as HTMLInputElement;
 
     const reviewValue = reviewInput.value;
-    const ratingValue = parseInt(ratingInput.value);
+    // const ratingValue = parseInt(ratingInput.value);
+    const ratingValue = rating;
 
     if (!ratingValue) {
-      setErrorMessage("Rating is required.")
+      setErrorMessage('Rating is required.')
       return;
     }
 
-    if (ratingValue < 1 || ratingValue > 5) {
-      setErrorMessage("Rating must be between 1 and 5.")
-      return;
-    }
+    // if (ratingValue < 1 || ratingValue > 5) {
+    //   setErrorMessage('Rating must be between 1 and 5.')
+    //   return;
+    // }
 
     const newReview: IReview = {
       review: reviewValue,
@@ -36,25 +39,27 @@ export const ReviewForm = ({ onAdd }: IReviewFormProps) => {
     };
 
     onAdd(newReview);
-    reviewInput.value = "";
-    ratingInput.value = "";
-    setErrorMessage("");
+    reviewInput.value = '';
+    // ratingInput.value = '';
+    setRating(0);
+    setErrorMessage('');
   };
-
+  
   return (
-    <Flex direction="column" gap={3} alignItems="flex-start" mt={10}>
-      <Textarea id="review-input" variant="outline" placeholder="Add review" width='100%' backgroundColor='aliceblue' textColor='#1a1a1a' />
-      <Input
-        id="rating-input"
-        type="number"
+    <Flex direction='column' gap={5} alignItems='flex-start' mt={10}>
+      <Textarea id='review-input' variant='outline' placeholder='Add review' width='100%' backgroundColor='aliceblue' textColor='#1a1a1a' />
+      {/* <Input
+        id='rating-input'
+        type='number'
         min={1}
         max={5}
-        variant="outline"
-        placeholder="Add rating"
+        variant='outline'
+        placeholder='Add rating'
         width='20%'
         backgroundColor='aliceblue'
         textColor='#1a1a1a'
-      />
+      /> */}
+      <StarRating defaultValue={rating} onChange={setRating} mode={2} />
       {errorMessage && (<Text color='red' fontSize='sm'>{errorMessage}</Text>)}
       <Button onClick={onClickHandler}>Post</Button>
     </Flex>
